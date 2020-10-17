f148311167b6cea7e833cbaaac6b8a93d5f2008e
unknown <richeti1998@gmail.com>
2020-10-17 15:38:18 -0500

    login2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('port',process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
 
 app.use(require('./controller/authController'));
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('./database');
 
 async function init(){
-    await app.listen(3000);
+    await app.listen(app.get('port'));
     console.log('Server on port 3000');
 
 }
